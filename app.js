4a1aed66173278e8f684f0f22e8b5d8ced9261eb
RikuHirose <rikuparkour9@gmail.com>
2019-03-04 17:21:02 +0700

    api/get-tinder-token

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var tinderAuth = require('./routes/tinderAuth');
+// var autoLike = require('./routes/autoLike');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tinderAuth', tinderAuth);
+// app.use('/autoLike', autoLike);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
